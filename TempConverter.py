bc09f193ada282be5ca3aaa76b10e77409c893f2
Carlo Mendoza <mendoza_carlo05@yahoo.com>
2017-10-30 18:31:24 +0800

    Added convert to Celsius function

--- TempConverter.py
+++ TempConverter.py
@@ -2,6 +2,12 @@ def convertToF(tempCel):
 	tempFar = (tempCel * 9)/5 + 32
 	return tempFar
 
+def convertToC(tempFar):
+	tempCel = (tempFar - 32) / (9/5)
+	return tempCel
+
 if __name__ == '__main__':
 	for i in range(20):
 		print(convertToF(i))
+	for i in range(20):
+		print(convertToC(i))
